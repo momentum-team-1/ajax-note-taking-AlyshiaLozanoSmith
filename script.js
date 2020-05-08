b83fb362eb7426e9e8c05ad28f2bc6465e534f21
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-05-08 17:31:21 -0400

    created html and got started on js

---
+++ script.js
@@ -0,0 +1,15 @@
+//add new notes
+//edit notes
+//delete notes
+//notes include: title, text, and timestamp most recently updated
+//add priority?
+//style as boxes?
+
+let noteForm = document.querySelector('#notepad-form')
+
+noteForm.addEventListener('submit', function(){
+    console.log('the thing happened')
+})
+
+
+
