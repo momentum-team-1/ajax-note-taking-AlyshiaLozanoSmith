0e66b124e85b2d67b9e68e9fd2cf516acec22381
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-05-11 09:23:18 -0400

    had a real rough time with this one. added functions for adding new notes and rendering on the page. that was as far as I got

--- script.js
+++ script.js
@@ -6,10 +6,49 @@
 //style as boxes?
 
 let noteForm = document.querySelector('#notepad-form')
+let notepadText = document.getElementById('notepad-text')
+let noteValue = notepadText.value
+
+
 
 noteForm.addEventListener('submit', function(){
-    console.log('the thing happened')
+    event.preventDefault()
+    let notepadText = document.getElementById('notepad-text')
+    let noteValue = notepadText.value 
+    let titleText = document.getElementById('title-text')
+    let titleValue = titleText.value
+    //create new note in database
+    notepadText.value = ''
+    titleText.value = ''
+    createNewNote(titleValue, noteValue)
 })
 
+//write fetch request to POST data
+function createNewNote(titleContent, noteContent){
+    return fetch('http://localhost:3000/notes/', {
+        method: 'POST' ,
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({ title: titleContent, item : noteContent , createdAt: 'moment().format()'}
+        )
+    })
+    .then((response) => {
+        return response.json()
+    })
 
+}
 
+function renderNotes(){
+    notelist.innerHTML = ''
+    fetch('http://localhost:3000/notes/', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(function(data){
+        let list = document.createElement('ul')
+        for (let item of data)
+        let listItem = document.createElement('li') 
+        listItem.dataset.id = item.id
+        listitem.innertext = item.item
+        list.appendChild(listItem)
+    })
+}
